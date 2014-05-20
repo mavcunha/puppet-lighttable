41d5afc6c73306025d370a8f20aa0b5986aa7bdf
Marco Valtas <mavcunha@gmail.com>
2014-05-20 08:18:01 -0300

    Capitalize, link light bin and set correct permissions

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,20 @@
 # Public: Installs LightTable
 class lighttable ($version = '0.6.5')  {
-  package { 'lighttable':
+  package { 'LightTable':
     source   => "https://d35ac8ww5dfjyg.cloudfront.net/playground/bins/${version}/LightTableMac.zip",
     provider => 'compressed_app',
+  }
+  file { '/usr/local/bin/light':
+    ensure  => 'link',
+    target  => '/Applications/LightTable/light',
+    require => Package['LightTable'],
+  }
+  # plugins are downloaded locally to LightTable directory
+  # we got have write permissions
+  file { '/Applications/LightTable':
+    recurse => true,
+    owner   => $::boxen_user,
+    group   => 'staff',
+    require => Package['LightTable'],
   }
 }
--- spec/classes/lighttable_spec.rb
+++ spec/classes/lighttable_spec.rb
@@ -2,16 +2,37 @@ require 'spec_helper'
 
 describe 'lighttable' do
   context 'no version given' do
-    it { should contain_package('lighttable').with({
+    it { should contain_package('LightTable').with({
       :source => 'https://d35ac8ww5dfjyg.cloudfront.net/playground/bins/0.6.5/LightTableMac.zip',
       :provider => 'compressed_app',
     })}
   end
   context 'version given' do
     let(:params) {{ :version => '0.6.0' }}
-    it { should contain_package('lighttable').with({
+    it { should contain_package('LightTable').with({
       :source => 'https://d35ac8ww5dfjyg.cloudfront.net/playground/bins/0.6.0/LightTableMac.zip',
       :provider => 'compressed_app',
+    })}
+  end
+  describe 'creates link to light for cmd line' do
+    it { should contain_file('/usr/local/bin/light').with({
+      :ensure => 'link',
+      :target => '/Applications/LightTable/light',
+      :require => 'Package[LightTable]',
+    })}
+  end
+  describe 'sets owner and group to LightTable directory' do
+    let(:boxenuser) { 'github_user' }
+    let(:facts) do
+      {
+        :boxen_user => 'boxenuser'
+      }
+    end
+    it { should contain_file('/Applications/LightTable').with({
+      :owner   => 'boxenuser',
+      :group   => 'staff',
+      :recurse => 'true',
+      :require => 'Package[LightTable]',
     })}
   end
 end
