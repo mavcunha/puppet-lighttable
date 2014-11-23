bf769cdad7a83f25f72a6c150cee11922ad13576
Marco Valtas <mavcunha@gmail.com>
2014-11-23 17:22:53 -0200

    Compressed_app has changed and it doesn't work anymore

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,20 +1,32 @@
 # Public: Installs LightTable
-class lighttable ($version = '0.6.5')  {
-  package { 'LightTable':
-    source   => "https://d35ac8ww5dfjyg.cloudfront.net/playground/bins/${version}/LightTableMac.zip",
-    provider => 'compressed_app',
+class lighttable ($version = '0.7.2')  {
+
+  $lighttable_source = "https://d35ac8ww5dfjyg.cloudfront.net/playground/bins/${version}/LightTableMac.zip"
+  $cached_source = '/tmp/LightTableMac.zip'
+
+  exec { 'LightTableDownload':
+    command => "/usr/bin/curl ${lighttable_source} -Lqo ${cached_source}",
+    creates => '/Applications/LightTable/LightTable.app',
   }
+
+  exec { 'LightTableUnzip':
+    command => "/usr/bin/ditto -xk ${cached_source} /Applications",
+    require => Exec['LightTableDownload'],
+    onlyif  => "/bin/ls ${cached_source} &> /dev/null",
+    creates => '/Applications/LightTable/LightTable.app',
+    notify  => [File['/Applications/LightTable'], File['/usr/local/bin/light']],
+  }
+
   file { '/usr/local/bin/light':
     ensure  => 'link',
     target  => '/Applications/LightTable/light',
-    require => Package['LightTable'],
   }
+
   # plugins are downloaded locally to LightTable directory
   # we got have write permissions
   file { '/Applications/LightTable':
     recurse => true,
     owner   => $::boxen_user,
-    group   => 'staff',
-    require => Package['LightTable'],
+    group   => 'admin',
   }
 }
--- spec/classes/lighttable_spec.rb
+++ spec/classes/lighttable_spec.rb
@@ -1,24 +1,10 @@
 require 'spec_helper'
 
 describe 'lighttable' do
-  context 'no version given' do
-    it { should contain_package('LightTable').with({
-      :source => 'https://d35ac8ww5dfjyg.cloudfront.net/playground/bins/0.6.5/LightTableMac.zip',
-      :provider => 'compressed_app',
-    })}
-  end
-  context 'version given' do
-    let(:params) {{ :version => '0.6.0' }}
-    it { should contain_package('LightTable').with({
-      :source => 'https://d35ac8ww5dfjyg.cloudfront.net/playground/bins/0.6.0/LightTableMac.zip',
-      :provider => 'compressed_app',
-    })}
-  end
   describe 'creates link to light for cmd line' do
     it { should contain_file('/usr/local/bin/light').with({
       :ensure => 'link',
       :target => '/Applications/LightTable/light',
-      :require => 'Package[LightTable]',
     })}
   end
   describe 'sets owner and group to LightTable directory' do
@@ -30,9 +16,8 @@ describe 'lighttable' do
     end
     it { should contain_file('/Applications/LightTable').with({
       :owner   => 'boxenuser',
-      :group   => 'staff',
+      :group   => 'admin',
       :recurse => 'true',
-      :require => 'Package[LightTable]',
     })}
   end
 end
